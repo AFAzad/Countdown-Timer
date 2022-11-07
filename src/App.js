a34193195106c9be127269aa1fc26c592fe470ae
Abul Fajle Azad <abulfajle405@gmail.com>
2022-11-07 23:53:47 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import React , {useState}from "react";
 
-function App() {
+import "./App.css";
+
+const App = () => {
+	const [timeDays,settimeDays]=useState('00');
+	const [timerhours,setTimerHours]=useState('00');
+	const [timerMin,setTimerMin]=useState('00');
+	const [timeSec,setTimeSec]=useState('00');
+
+	let interval = useRef();
+
+	const starttimer =()=>{
+		const Countdowndate =  new Date("Nov 08, 2022 00: 00: 00").getTime();
+		interval = setInterval(()=>{
+			const now = new Date().getTime();
+			const distance = Countdowndate - now;
+
+			
+		},1000)
+	}
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+	
+    <section className="main-section">
+      
+      
+        <section className="App">
+			<div>
+        <span className="countspan"></span>
+        <h2>Countdown Timer</h2>
+        {/* <p>
+          Countdown to really special date. One you could or never imagine !
+        </p> */}
+      </div></section>
+	  <section className="timer">
+	  <div>
+          <section>
+            <p>87</p>
+            <p>
+              <small>Days</small>
+            </p>
+          </section>
+          <span>:</span>
+          <section>
+            <p>87</p>
+            <p>
+              <small>Hours</small>
+            </p>
+          </section>
+          <span>:</span>
+
+          <section>
+            <p>87</p>
+            <p>
+              <small>Minutes</small>
+            </p>
+          </section>
+          <span>:</span>
+
+          <section>
+            <p>87</p>
+            <p>
+              <small>Seconds</small>
+            </p>
+          </section>
+          <span></span>
+        </div>
+	  </section>
+      
+    </section>
   );
-}
+};
 
 export default App;
